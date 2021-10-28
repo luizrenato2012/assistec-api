1374a2bb7622fbe098abf1d747c226196d8e6e34
Luiz Renato <luizrenato2005@gmail.com>
2021-10-28 01:04:53 -0300

    Implementado validacao de Usuarios

--- src/modules/usuarios/routes/usuarios.routes.ts
+++ src/modules/usuarios/routes/usuarios.routes.ts
@@ -1,10 +1,22 @@
+import { celebrate, Segments } from 'celebrate';
 import { Router } from 'express';
+import Joi from 'joi';
 import UsuariosController from '../controllers/UsuariosController';
 
 const usuariosRoutes = Router();
 const usuariosController = new UsuariosController();
 
 usuariosRoutes.get('/', usuariosController.index);
-usuariosRoutes.post('/', usuariosController.create);
+usuariosRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usuariosController.create,
+);
 
 export default usuariosRoutes;
